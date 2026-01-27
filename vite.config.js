42cde0ea7169e2fd9070bba39c3fd110ed515754
Kumaran M <kumaran@qoruz.com>
2026-01-27 18:43:19 +0530

    bug fixed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslintPlugin({
+      // ignore errors/warnings during build
+      failOnError: false,
+      failOnWarning: false,
+      emitWarning: false,
+      emitError: false,
+    }),
+  ],
   build: {
     outDir: "build",
   },
